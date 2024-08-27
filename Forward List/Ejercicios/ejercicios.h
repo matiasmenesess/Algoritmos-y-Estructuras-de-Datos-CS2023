661f528ecab53af542c82131b5f3ed4b926a10ce
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-08-26 20:04:24 -0500

    ejercicios.h

---
+++ Forward List/Ejercicios/ejercicios.h
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+struct Node{
+    T data;
+    Node * next= nullptr;
+};
+
+template<typename T>
+class List{
+    Node<T> * head = nullptr;
+
+public:
+
+    void Invertir(){ //O(n)
+
+        Node<T>* anterior = nullptr;
+        auto *actual =head;
+        Node<T>* posterior = nullptr;
+        while(actual!= nullptr){
+            posterior = actual->next;
+            actual->next = anterior;
+            anterior = actual;
+            actual = posterior;
+        }
+    }
+
+
+
+};
+
+template<typename T>
+void Invertir_Como_Funcion(List<T> lista){ //O(n)
+
+    Node<T>* anterior = nullptr;
+    auto *actual =lista.head;
+    Node<T>* posterior = nullptr;
+    while(actual!= nullptr){
+        posterior = actual->next;
+        actual->next = anterior;
+        anterior = actual;
+        actual = posterior;
+    }
+    return;
+};
+
+template<typename T>
+T Nueva_Lista_Ordenada(List<T> lista1, List<T> lista2){
+
+}
+
+template<typename T>
+T interseccion(List<T> lista1, List<T> lista2){
+
+}
+
+
+
+
+
+
+
