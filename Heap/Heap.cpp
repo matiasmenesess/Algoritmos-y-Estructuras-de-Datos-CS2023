14748356f60a483e62c1cbe2850f76ab1b8d891a
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-10-12 19:38:33 -0500

    plantilal actualizada y mejkorada

--- Heap/Heap.cpp
+++ Heap/Heap.cpp
@@ -3,31 +3,21 @@
 using namespace std;
 
 template<typename T>
-struct Node {
-    Node<T>* left = nullptr;
-    Node<T>* right = nullptr;
-    int index;
-    T val;
-
-    Node(int index, T val): index(index), val(val) {}
-};
-
-template<typename T>
 class Heap {
     string type;
     vector<T> elements;
 
     void heapify_down(int i) {
-        int left = 2 * i + 1; //hijo izquierdo
-        int right = 2 * i + 2; // hijo derecho
-        int largest = i; //padre (se pone largest pero podria ser menor en un min_heap)
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
 
         if (type == "MAX_HEAP") {
-            if (left < elements.size() && elements[left] > elements[largest]) { //si existe el hijo y es mayor que el padre
-                largest = left; //el indice del mas grande es el del hijo izq
+            if (left < elements.size() && elements[left] > elements[largest]) {
+                largest = left;
             }
             if (right < elements.size() && elements[right] > elements[largest]) {
-                largest = right; //el indice del mas grande es el del hijo der
+                largest = right;
             }
         } else if (type == "MIN_HEAP") {
             if (left < elements.size() && elements[left] < elements[largest]) {
@@ -38,39 +28,27 @@ class Heap {
             }
         }
 
-        if (largest != i) { //si el largest no es el padre
-            swap(elements[i], elements[largest]); // intercambia el valor del padre con el del mas grande (o mas chiquito)
-            heapify_down(largest); // como no sabemos si la propiedad del heap se sigue cumpliendo después del
-            // intercambio, aplicamos recursividad en el nuevo índice
+        if (largest != i) {
+            swap(elements[i], elements[largest]);
+            heapify_down(largest);
         }
     }
 
     void heapify_up(int i) {
-        int parent = (i - 1) / 2; // indidce del padre
-
-        // mientras no hayamos llegado a la raíz y la propiedad del heap no se cumpla, intercambiamos con el padre
+        int parent = (i - 1) / 2;
         if (i && ((type == "MAX_HEAP" && elements[i] > elements[parent]) ||
                   (type == "MIN_HEAP" && elements[i] < elements[parent]))) {
             swap(elements[i], elements[parent]);
-            heapify_up(parent); //repetimos el proceso hacia arriba
+            heapify_up(parent);
         }
     }
 
 public:
     Heap(string heapType) : type(heapType) {}
 
-
-
     void insert(T val) {
         elements.push_back(val);
         heapify_up(elements.size() - 1);
-    }
-
-    void display() {
-        for (const auto& el : elements) {
-            cout << el << " ";
-        }
-        cout << endl;
     }
 
     void pop() {
@@ -78,45 +56,19 @@ public:
             cout << "Heap vacio" << endl;
             return;
         }
-
         swap(elements[0], elements[elements.size() - 1]);
         elements.pop_back();
         heapify_down(0);
     }
 
-    void build(T* arr, int n) { //se le pasa el array con elementos
-        elements.assign(arr, arr + n); //copia los elementos del array al vector
-        buildHeap();
+    T top() {
+        if (elements.size() == 0) {
+            throw out_of_range("Heap vacio");
+        }
+        return elements[0];
     }
 
-    void buildHeap() {
-        int index = (elements.size() / 2) - 1; //nos da el ultimo nodo no hoja
-        while (index >= 0) {
-            heapify_down(index);
-            index--;
-        }
+    int size() {
+        return elements.size();
     }
 };
-
-int main() {
-
-  //ejemplo 
-    int arr[] = {3, 19, 1, 14, 8, 7};
-    int n = sizeof(arr) / sizeof(arr[0]); //sizeof da el size de algo
-
-    Heap<int> maxHeap("MAX_HEAP");
-    maxHeap.build(arr, n);
-
-    cout << "mostrar : ";
-    maxHeap.display();
-
-    maxHeap.insert(25);
-    cout << "Heap despues de insertar 25: ";
-    maxHeap.display();
-
-    maxHeap.pop();
-    cout << "Heap despues de hacer pop: ";
-    maxHeap.display();
-
-    return 0;
-}
